5b18c7b0b7a271bd3f321230793e429f898429aa
Xmkkss <3156896114@qq.com>
2026-04-18 03:44:31 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.mp3', '**/*.flac', '**/*.wav', '**/*.ogg'],
+  base: '/Personal_web_vibe_coding/',
 })
